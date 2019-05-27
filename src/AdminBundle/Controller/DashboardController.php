5501dd1058415c70e4ca89191583895cfeb62184
Valentin Carrie <valentin@definima.com>
2019-05-27 09:04:35 +0200

    remove lrenkg

--- src/AdminBundle/Controller/DashboardController.php
+++ src/AdminBundle/Controller/DashboardController.php
@@ -11,7 +11,6 @@ class DashboardController extends Controller {
     public function indexAction() {
         
         $cr = $this->getUser();
-        $this->addFlash('success', "lrenkg");
 
         $bundles = DashboardController::get_menus();
         return $this->render('AdminBundle:Dashboard:index.html.twig', array(
